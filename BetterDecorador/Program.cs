df38202e2b5bcd3c658ec3f5e57ef9c9f0f81c1e
Daniel <danielmordelfer@gmail.com>
2025-03-14 13:12:01 +0000

    Fixed the args2 parse from char to int

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -7,9 +7,9 @@ namespace BetterDecorador
         private static void Main(string[] args)
         {
             char character = char.Parse(args[1]);
-            char number = char.Parse(args[2]);
+            int number = int.Parse(args[2]);
 
-            Console.Write(Decor(args[0],character,number));
+            Console.WriteLine(Decor(args[0],character,number));
         }
 
         private static string Decor(string s, char dec, int space)
