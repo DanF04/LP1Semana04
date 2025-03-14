5efdb55576b7905d0ca6eacbdfa7a2c1e52f7207
Daniel <danielmordelfer@gmail.com>
2025-03-14 14:06:29 +0000

    Program that guves the number in a special sequence through the user input

---
+++ Special/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Special
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            int input = int.Parse(Console.ReadLine());
+            // 0, 1, 1, 3, 5, 11, 21, 43, 85, 171, 341, ...
+            
+            Console.WriteLine(GetSpecial(input));
+            
+        }
+
+        private static int GetSpecial (int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+
+            else 
+            return GetSpecial(n-1) + (2 * GetSpecial(n-2));
+            
+
+        }
+    }
+}
