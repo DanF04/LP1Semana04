4ebf2fa0a25acb5898bb308dd8f898d0f00a7682
Daniel <danielmordelfer@gmail.com>
2025-03-14 13:11:15 +0000

    New program that writes a text and a char, those chars depend on the user input

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            char character = char.Parse(args[1]);
+            char number = char.Parse(args[2]);
+
+            Console.Write(Decor(args[0],character,number));
+        }
+
+        private static string Decor(string s, char dec, int space)
+        {
+            string decs = "";
+            for (int i = 0; i<space; i++)
+            {
+                decs += dec;
+            }
+
+            return $"{decs} {s} {decs}";
+        }
+    }
+}
